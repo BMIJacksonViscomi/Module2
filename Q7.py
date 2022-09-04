6de89132852610fba63ba0d1080cc5069286a971
BMIJacksonViscomi <u1437829@umail.utah.edu>
2022-09-04 14:43:10 -0600

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -1,2 +1,4 @@
-Medication = 5
-type(Medication)
+#Question 7: Jack's Medications 
+#Jack has been prescribed 5 medications
+Medication = 5 #assinging 5 to Medication variable 
+type(Medication) #printing the type of variable assigned to Medication
